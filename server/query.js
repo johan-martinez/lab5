bdadd52c5f290d2af9668906dbb0cd5035abf556
AndresFw <andres.viviescas@uptc.edu.co>
2021-04-16 21:07:13 -0500

    queries are updated

--- server/query.js
+++ server/query.js
@@ -6,29 +6,49 @@ let servers = [{ server: "http://localhost:3000", isLeader: true, id: 3000 },
 
 let id = process.env.ID;
 
-function getLeader() {
-    return servers.find(x => x.isLeader)
+async function getLeader() {
+    global.myServer = await Server.find({isLeader:{$eq:true}}, 'server', {}, (err, docs)=>{
+        return err ? [] : docs;
+    })
+   // return servers.find(x => x.isLeader)
 }
 
-function getMyInfo() {
-    global.myServer = servers.find(x => x.id == id)
+async function getMyInfo() {
+    global.myServer = await Server.find({id:{$eq:this.id}}, 'server', {}, (err, docs)=>{
+        return err ? [] : docs;
+    })
+
+//    global.myServer = servers.find(x => x.id == id)
 }
 
-function getMajors() {
-    return servers.filter(x => x.id > id);
+async function getMajors() {
+    return await Server.find({id:{$eq:this.id}}, 'server', {}, (err, docs)=>{
+        return err ? [] : docs;
+    })
+
+   // return servers.filter(x => x.id > id);
 }
 
-function getUrls() {
+async function getUrls() {
     
-    Server.find({}, 'server', {}, (err, docs)=>{
+    await Server.find({}, 'server', {}, (err, docs)=>{
         return err ? [] : docs;
     })
 }
 
-function updateLeader() {
-    let leader =  servers.find(x => x.isLeader)
-    if(leader) leader.isLeader = false
-    servers.find(x => x.id == id).isLeader = true;
+async function updateLeader() {
+    //let leader =  servers.find(x => x.isLeader)
+   // if(leader) leader.isLeader = false
+    //servers.find(x => x.id == id).isLeader = true;
+    let leader = getLeader();
+    if(leader){
+        await Server.updateOne({isLeader:{$eq:true}}, {isLeader:false}, (err, docs)=>{
+            return err ? [] : docs;
+        })
+    } 
+    await Server.updateOne({id:{$eq:this.id}}, {isLeader:true}, (err, docs)=>{
+        return err ? [] : docs;
+    })
 }
 
 module.exports = {getLeader, getMajors, getUrls, getMyInfo, updateLeader};
